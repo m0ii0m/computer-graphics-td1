faf4fe84014a99bd60e0be3d4a23c09553c684aa
m0ii0m <ludovic.regnier.2004@gmail.com>
2026-03-19 13:58:11 +0100

    1.2

--- ex1.cpp
+++ ex1.cpp
@@ -3,16 +3,17 @@ void Render()
 {
   glClearColor(1.f, 1.f, 0.f, 1.f);
   glClear(GL_COLOR_BUFFER_BIT);
+  glEnable(GL_CULL_FACE);
   glBegin(GL_TRIANGLES);
 
   glColor3f(1.f, 0.f, 0.f);
   glVertex2f(0.0f, 0.5f);
 
-  glColor3f(0.f, 1.f, 0.f);
-  glVertex2f(0.5f, -0.5f);
-
   glColor3f(0.f, 0.f, 1.f);
   glVertex2f(-0.5f, -0.5f);
+
+  glColor3f(0.f, 1.f, 0.f);
+  glVertex2f(0.5f, -0.5f);
 
   glEnd();
 }
